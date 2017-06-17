17f57ece428e886d79677c0431975d35dbae6944
drammock <dan.mccloy@gmail.com>
2017-06-16 17:05:30 -0700

    avoids missing sentence/talker pairs; removes decimal from SNR in filename

--- counterbalance-trials.py
+++ counterbalance-trials.py
@@ -45,7 +45,7 @@ for subj in range(n_listeners):
     # save
     snr_assignments.extend(this_snrs)
 design_matrix['snr'] = snr_assignments
-design_matrix['snr'] = design_matrix['snr'].map(lambda x: '{:+.1f}'.format(x))
+design_matrix['snr'] = design_matrix['snr'].map(lambda x: '{:+d}'.format(x))
 
 # assign talkers to sentence/SNR pairings. Start at a different point in the
 # talker sequence **for each tiling of the talker list** (avoids problems that
@@ -79,6 +79,46 @@ assert all(design_matrix.groupby(['listener', 'talker', 'snr']).count().nunique(
 # "assert" statements failed, try changing the "== 1" to "<= 2". If that works,
 # then remove the "assert all" and ".nunique()..." parts of each line, and make
 # sure the .min() and .max() of each count are within 1 of each other.
+
+# check proposal against the list of missing sentence / talker combinations
+stims = (design_matrix['talker'] + '_' +
+         design_matrix['sentence']).values.astype(str)
+miss = pd.read_csv('missing-sentences.csv', header=0)
+missing_stims = (miss['talker'] + '_' + miss['sentence']).values.astype(str)
+missing_stim_indices = np.where(np.in1d(stims, missing_stims))[0]
+missing_trials = design_matrix.iloc[missing_stim_indices].copy()
+missing_talkers = missing_trials['talker'].values.astype(str)
+# for the missing talker/sentence combinations, shuffle the talker assignments
+# (preserving the relative number of each). The approach below may not work if
+# any(missing_trials.groupby('sentence').aggregate('nunique')['talker'] != 1)
+# (i.e., if some sentences are missing for >1 talker)
+# NOTE: value_counts() defaults to descending order, which is in our favor here
+missing_talker_counts = missing_trials['talker'].value_counts()
+shuffled_talkers = np.full_like(missing_talkers, '')
+converged, iteration, max_iter = False, 1, 1000
+while iteration < max_iter and not converged:
+    for talker, count in missing_talker_counts.iteritems():
+        legal_indices = np.where(missing_talkers != talker)[0]
+        empty_indices = np.where(shuffled_talkers == '')[0]
+        available_indices = np.intersect1d(legal_indices, empty_indices)
+        try:
+            ixs = rand.choice(available_indices, count, replace=False)
+            shuffled_talkers[ixs] = talker
+            converged = True
+        except ValueError:
+            iteration += 1
+success = ['resolved', 'could not resolve'][int(iteration == max_iter)]
+plural = ['', 's'][int(iteration > 1)]
+message = '{} missing sentences in {} iteration{}.'.format(success, iteration,
+                                                           plural)
+if converged:
+    print(message)
+else:
+    raise RuntimeError(message)
+# assign the shuffled talker list into the indices of the design matrix that
+# were problematic
+design_matrix.loc[design_matrix.index[missing_stim_indices],
+                  'talker'] = shuffled_talkers
 
 # add a filename column
 design_matrix['filename'] = (design_matrix[['talker', 'sentence',
